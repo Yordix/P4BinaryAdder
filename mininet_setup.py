93a705bc90944d3054cc03ca95ae98956b9f0f44
Yordix <norbert2923@gmail.com>
2024-06-29 23:14:50 +0200

    Added mininet setup and p4app.json

---
+++ mininet_setup.py
@@ -0,0 +1,39 @@
+from mininet.net import Mininet
+from mininet.node import RemoteController, OVSSwitch
+from mininet.link import TCLink
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
+def setup():
+    net = Mininet(controller=RemoteController, link=TCLink, switch=OVSSwitch)
+
+    info('*** Adding controller\n')
+    net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6653)
+
+    info('*** Adding hosts\n')
+    h1 = net.addHost('h1', ip='10.0.0.1/24')
+    h2 = net.addHost('h2', ip='10.0.0.2/24')
+
+    info('*** Adding switch\n')
+    s1 = net.addSwitch('s1')
+
+    info('*** Creating links\n')
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+
+    info('*** Starting network\n')
+    net.start()
+
+    info('*** Configuring hosts\n')
+    h1.cmd('ifconfig h1-eth0 10.0.0.1/24')
+    h2.cmd('ifconfig h2-eth0 10.0.0.2/24')
+
+    info('*** Running CLI\n')
+    CLI(net)
+
+    info('*** Stopping network\n')
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    setup()
